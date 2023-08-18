3dc4a5937bd5cd2ad12db5083c29048422b7567a
Vincent <vincentwilliams97@gmail.com>
2023-08-18 13:22:18 -0700

    feat: Setup Apollo graphql client

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   id(libs.plugins.androidApplication.get().pluginId) apply false
   id(libs.plugins.androidLibrary.get().pluginId) apply false
+  id(libs.plugins.apollo.get().pluginId).version(libs.plugins.apollo.get().version.toString()) apply false
   id(libs.plugins.kotlinAndroid.get().pluginId) apply false
 }
