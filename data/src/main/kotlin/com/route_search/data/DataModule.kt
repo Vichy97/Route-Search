03f58c399b37c442524e316a5b168df84f2e4719
Vincent <vincentwilliams97@gmail.com>
2023-08-19 18:13:26 -0700

    feat: Add area repository

--- data/src/main/kotlin/com/route_search/data/DataModule.kt
+++ data/src/main/kotlin/com/route_search/data/DataModule.kt
@@ -1,8 +1,11 @@
 package com.route_search.data
 
 import com.route_search.network.networkModule
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dataModule = networkModule + module {
 
+  singleOf(::DefaultAreaRepository) bind AreaRepository::class
 }
