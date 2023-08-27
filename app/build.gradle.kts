8f1374dc7f05de46714f95d2b90d04c7e0ebdcd2
Vincent <vincentwilliams97@gmail.com>
2023-08-27 13:09:56 -0700

    feat: Set up Leak Canary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
   implementation(libs.ui.tooling.preview)
   implementation(libs.material3)
 
+  debugImplementation(libs.leakcanary.android)
   debugImplementation(libs.ui.tooling)
   debugImplementation(libs.ui.test.manifest)
 
