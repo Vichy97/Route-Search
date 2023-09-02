df8ee272e9a089ca33452eb3b0bc7dd79e0aa1d8
Vincent <vincentwilliams97@gmail.com>
2023-09-02 11:17:56 -0700

    chore: Rename network koin module to be more consistent

--- data/remote/src/main/kotlin/com/routesearch/network/RemoteDataModule.kt
+++ data/remote/src/main/kotlin/com/routesearch/network/RemoteDataModule.kt
@@ -7,7 +7,7 @@ import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
-val networkModule = module {
+val remoteDataModule = module {
 
   single<ApolloClient> {
     ApolloClient.Builder()
