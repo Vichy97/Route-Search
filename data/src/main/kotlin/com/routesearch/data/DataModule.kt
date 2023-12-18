e7168269e2429e6996b85d912637fa8d540b9f68
Vincent <vincentwilliams97@gmail.com>
2023-12-17 20:42:38 -0700

    chore: Update DefaultAreaRepository to run on IO Dispatcher

--- data/src/main/kotlin/com/routesearch/data/DataModule.kt
+++ data/src/main/kotlin/com/routesearch/data/DataModule.kt
@@ -2,11 +2,14 @@ package com.routesearch.data
 
 import com.routesearch.data.area.AreaRepository
 import com.routesearch.data.area.DefaultAreaRepository
+import com.routesearch.data.local.area.AreaLocalDataSource
 import com.routesearch.data.local.localDataModule
+import com.routesearch.data.remote.area.AreaRemoteDataSource
 import com.routesearch.data.remote.remoteDataModule
-import org.koin.core.module.dsl.singleOf
+import com.routesearch.util.coroutines.IoContext
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import kotlin.coroutines.CoroutineContext
 
 val dataModule = module {
 
@@ -15,5 +18,11 @@ val dataModule = module {
     localDataModule,
   )
 
-  singleOf(::DefaultAreaRepository) bind AreaRepository::class
+  single {
+    DefaultAreaRepository(
+      remoteDataSource = get<AreaRemoteDataSource>(),
+      localDataSource = get<AreaLocalDataSource>(),
+      coroutineContext = get<CoroutineContext>(IoContext),
+    )
+  } bind AreaRepository::class
 }
--- data/src/main/kotlin/com/routesearch/data/area/DefaultAreaRepository.kt
+++ data/src/main/kotlin/com/routesearch/data/area/DefaultAreaRepository.kt
@@ -2,15 +2,20 @@ package com.routesearch.data.area
 
 import com.routesearch.data.local.area.AreaLocalDataSource
 import com.routesearch.data.remote.area.AreaRemoteDataSource
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
 internal class DefaultAreaRepository(
   private val remoteDataSource: AreaRemoteDataSource,
   private val localDataSource: AreaLocalDataSource,
+  private val coroutineContext: CoroutineContext,
 ) : AreaRepository {
 
-  override suspend fun getArea(id: String) = localDataSource.getArea(id)
-    .map { it.toArea() }
-    .recoverCatching { getAreaFromRemote(id) }
+  override suspend fun getArea(id: String) = withContext(coroutineContext) {
+    localDataSource.getArea(id)
+      .map { it.toArea() }
+      .recoverCatching { getAreaFromRemote(id) }
+  }
 
   private suspend fun getAreaFromRemote(id: String) = remoteDataSource.getArea(id)
     .map { it.toArea() }
