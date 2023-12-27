1f85fc486915bf85a1fb8d1440e43ec44b1fb621
Vincent <vincentwilliams97@gmail.com>
2023-12-26 23:03:10 -0700

    feat: Update Room Climb object to include new fields

--- data/local/src/main/kotlin/com/routesearch/data/local/climb/Climb.kt
+++ data/local/src/main/kotlin/com/routesearch/data/local/climb/Climb.kt
@@ -3,19 +3,61 @@ package com.routesearch.data.local.climb
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Relation
 
 @Entity(tableName = "climbs")
 data class Climb(
   @PrimaryKey
   val id: String,
   val areaId: String,
+  @Embedded
+  val metadata: MetaData,
   val name: String,
+  val pathTokens: List<String>,
+  @Embedded
+  val location: Location?,
+  val ancestorIds: List<String>,
+  @Embedded
+  val description: Description,
+  val length: Int,
+  val boltCount: Int?,
+  val fa: String,
   val type: String,
   @Embedded
   val grades: Grades?,
-)
+  val media: List<String>,
+) {
+
+  data class MetaData(
+    val leftRightIndex: Int?,
+    val createdAt: String,
+    val updatedAt: String,
+  )
+
+  data class Description(
+    val general: String,
+    val location: String,
+    val protection: String,
+  )
+}
 
 data class Grades(
   val yds: String?,
   val vScale: String?,
+)
+
+data class Location(
+  val latitude: Double,
+  val longitude: Double,
+)
+
+data class ClimbWithPitches(
+  @Embedded
+  val climb: Climb,
+  @Relation(
+    entity = Pitch::class,
+    parentColumn = "id",
+    entityColumn = "climbId",
+  )
+  val pitches: List<Pitch>,
 )
---
+++ data/local/src/main/kotlin/com/routesearch/data/local/climb/Pitch.kt
@@ -0,0 +1,19 @@
+package com.routesearch.data.local.climb
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "pitches")
+data class Pitch(
+  @PrimaryKey(autoGenerate = true)
+  val id: String,
+  val climbId: String,
+  val pitchNumber: Int,
+  val description: String,
+  val length: Int?,
+  val boltCount: Int?,
+  @Embedded
+  val grades: Grades?,
+  val type: String?,
+)
