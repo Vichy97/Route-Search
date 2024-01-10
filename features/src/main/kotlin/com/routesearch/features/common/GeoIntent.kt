7398b7122d6fc93ac48de4b0cb5b48ceb94fa0ff
Vincent <vincentwilliams97@gmail.com>
2024-01-09 20:51:01 -0700

    feat: Add GeoIntent
    Convenience class for launching a maps Intent

---
+++ features/src/main/kotlin/com/routesearch/features/common/GeoIntent.kt
@@ -0,0 +1,14 @@
+package com.routesearch.features.common
+
+import android.content.Intent
+import com.routesearch.data.location.Location
+
+class GeoIntent(
+  location: Location,
+  name: String,
+) : Intent(
+  parseUri(
+    "geo:0,0?q=${location.latitude},${location.longitude}($name)",
+    URI_ALLOW_UNSAFE,
+  ).addFlags(FLAG_ACTIVITY_NEW_TASK),
+)
