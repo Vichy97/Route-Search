cf1c58724f079544830365f8d6a00e548cc1a072
Vincent <vincentwilliams97@gmail.com>
2024-02-12 09:51:24 -0700

    chore: Mark AreaViewState classes as @Immutable
    This will allow Compose to skip recompositions for Composables that take the AreaViewState as a parameter

--- features/src/main/kotlin/com/routesearch/features/area/AreaViewState.kt
+++ features/src/main/kotlin/com/routesearch/features/area/AreaViewState.kt
@@ -1,14 +1,18 @@
 package com.routesearch.features.area
 
+import androidx.compose.runtime.Immutable
 import com.routesearch.data.area.Area
 
 internal sealed interface AreaViewState {
 
+  @Immutable
   data object Loading : AreaViewState
 
+  @Immutable
   data class Content(
     val area: Area,
   ) : AreaViewState
 
+  @Immutable
   data object Idle : AreaViewState
 }
