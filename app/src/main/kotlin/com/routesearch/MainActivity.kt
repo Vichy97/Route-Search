a0455745c4c8e006a85cf721fd7502cba4fa51a4
Vincent <vincentwilliams97@gmail.com>
2024-05-01 18:45:21 -0700

    fix: Fix snackbar not showing when requested
    This top level scaffold will exist only to host Snackbars and any other app-level ui like bottom nav or drawers that we might add in the future.

--- app/src/main/kotlin/com/routesearch/MainActivity.kt
+++ app/src/main/kotlin/com/routesearch/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.routesearch
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.routesearch.navigation.MainNavGraph
+import com.routesearch.ui.common.snackbar.SnackbarManager
 import com.routesearch.ui.common.theme.RouteSearchTheme
 import com.routesearch.util.view.configuration
 import com.routesearch.util.view.isInDarkMode
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.koin.android.ext.android.get
 
 class MainActivity : ComponentActivity() {
 
+  @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     window.configureSystemBars()
@@ -24,12 +36,28 @@ class MainActivity : ComponentActivity() {
 
     setContent {
       RouteSearchTheme {
+        val snackbarManager = get<SnackbarManager>()
+        val snackbarHostState = remember { SnackbarHostState() }
+        val context = LocalContext.current
         val navController = rememberNavController()
 
-        MainNavGraph(
-          modifier = Modifier.fillMaxSize(),
-          navController = navController,
-        )
+        LaunchedEffect("snackbar") {
+          snackbarManager.snackbarEvents.onEach {
+            val message = context.getString(it.message)
+            snackbarHostState.showSnackbar(message = message)
+          }.launchIn(this)
+        }
+
+        Scaffold(
+          snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+          },
+        ) {
+          MainNavGraph(
+            modifier = Modifier.fillMaxSize(),
+            navController = navController,
+          )
+        }
       }
     }
   }
